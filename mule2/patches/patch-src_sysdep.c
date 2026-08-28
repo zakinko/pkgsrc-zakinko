48377bbb2fed61ee5058b7a5a9bcc10b2b529a1e
Showta Ishizaki <zakinko@snowrabbit.org>
2026-08-28 16:26:34 +0900

    sysdep.c の終端は NULL ではなく (char *)0 で書く
    f989f80 で execlp の終端を (char *)NULL にしたが、このファイルは 27 行目で
    NULL を #undef していて、そのあと定義し直すヘッダを読まない。canna と wnn4 を
    入れた構成で 729 行目を通ったときに
    
    	sysdep.c:729:31: error: 'NULL' undeclared (first use in this function)
    
    で止まった。端末のみの構成では通らない条件だったので、入れたときには気づけて
    いない。
    
    (char *)0 なら NULL の定義に依存せず、ポインタとして 64 bit 積まれるので
    意図は変わらない。他の六本にこの #undef は無いので、そちらは NULL のまま。
    
    この木は 1995 年のもので、当時は NULL の定義が処理系ごとに揺れていた。自分で
    消して自分で決める、という書き方が残っている。新しく足すものが、その前提を
    踏まないようにする必要がある。

--- mule2/patches/patch-src_sysdep.c
+++ mule2/patches/patch-src_sysdep.c
@@ -8,6 +8,9 @@ whatever was left on the stack.  This is the same mistake as the XtVa*
 calls fixed elsewhere in this tree, in a place that has nothing to do with
 X: sys_subshell() spawning the user's shell.
 
+The cast is spelled (char *)0 rather than NULL because this file undefines
+NULL at line 27 and never pulls in a header that puts it back.
+
 --- src/sysdep.c.orig
 +++ src/sysdep.c
 @@ -725,7 +725,7 @@
@@ -15,7 +18,7 @@ X: sys_subshell() spawning the user's shell.
          report_file_error ("Can't execute subshell", Fcons (build_string (sh), Qnil));
  #else /* not MSDOS */
 -      execlp (sh, sh, 0);
-+      execlp (sh, sh, (char *)NULL);
++      execlp (sh, sh, (char *)0);
        write (1, "Can't execute subshell", 22);
        _exit (1);
  #endif /* not MSDOS */
