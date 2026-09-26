eb5354e94c70124a8b8a8963c5808db69160cafd
Showta Ishizaki <zakinko@snowrabbit.org>
2026-09-27 02:44:21 +0900

    elisp-compat: buildlink3.mk が上流の置き場を指していた
    BUILDLINK_PKGSRCDIR が ../../devel/elisp-compat のままだった。上流の
    木に devel/elisp-compat は無いので、この buildlink3.mk を読む package
    は依存を入れる段で行き先を失う。一番上の ../../zakinko/elisp-compat
    を指す。

--- elisp-compat/buildlink3.mk
+++ elisp-compat/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	elisp-compat
 ELISP_COMPAT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.elisp-compat+=	${EMACS_PKGNAME_PREFIX}elisp-compat>=20260924
-BUILDLINK_PKGSRCDIR.elisp-compat?=	../../devel/elisp-compat
+BUILDLINK_PKGSRCDIR.elisp-compat?=	../../zakinko/elisp-compat
 
 BUILDLINK_CONTENTS_FILTER.elisp-compat=	${EGREP} '.*\.el$$|.*\.elc$$'
 .endif # ELISP_COMPAT_BUILDLINK3_MK
